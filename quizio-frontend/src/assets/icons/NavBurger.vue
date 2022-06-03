bea43e8df3fdf543f0babbbfde3c643e60e24e4e
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-06-03 13:36:02 +0100

    Static assets added

---
+++ quizio-frontend/src/assets/icons/NavBurger.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navBurgerWrapper" @click='onClick'>
+        <svg width="30" height="18" viewBox="0 0 30 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="4" width="22" height="4" rx="1" fill="black"/>
+        <rect x="4" y="14" width="22" height="4" rx="1" fill="black"/>
+        <rect y="7" width="30" height="4" rx="1" fill="black"/>
+        </svg>
+    </div>
+</template>
+
+<style lang='scss' scoped>
+.navBurgerWrapper{
+    display:none;
+    cursor:pointer;
+}
+@media screen and (max-width:700px){
+    .navBurgerWrapper{
+        display:block;
+    }
+}
+</style>
+
+<script lang='ts'>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    props:{
+        onClick:Function
+    }
+})
+</script>
