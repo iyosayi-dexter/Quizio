0b439df5035616ed2846516d8949f707969bb687
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-06-03 13:36:43 +0100

    Set up routing with vue-router

--- quizio-frontend/src/router/index.ts
+++ quizio-frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter , createWebHistory } from "vue-router";
 import Home from '../views/Home.vue'
 
 
+
 const routes = [
     {
         path:'/',
@@ -11,47 +12,53 @@ const routes = [
     {
         path:'/dashboard',
         name:'Dashboard',
-        component:()=> import ('../views/Dashboard.vue')
+        component:()=> import('../views/Dashboard.vue')
     }
     ,
     {
         path:'/login',
         name:'Login',
-        component:()=> import ('../views/Login.vue')
+        component:()=> import('../views/Login.vue')
     },
     {
         path:'/signup',
         name:'SignUp',
-        component:()=> import ('../views/SignUp.vue')
+        component:()=> import('../views/SignUp.vue')
     },
     {
         path:'/messages',
         name:'Messages',
-        component:()=> import ('../views/Messages.vue')
+        component:()=> import('../views/Messages.vue')
     },
     {
         path:'/test',
         name:'Test',
-        component:()=> import ('../views/Test.vue')
+        component:()=> import('../views/Test.vue')
     },
     {
         path:'/tests',
         name:'Tests',
-        component:()=> import ('../views/Tests.vue')
+        component:()=> import('../views/Tests.vue')
     },
     {
         path:'/test-config',
         name:'Test-config',
-        component:()=> import ('../views/Test-config.vue')
+        component:()=> import('../views/Test-config.vue')
     },
-    {   path: '/404',
-        alias:'*',
-        component: ()=> import ('../views/NotFound.vue')
+    {   path: '/leaderboard',
+        name:'leaderboard',
+        component:()=> import('../views/Leaderboard.vue')
     },
     {
-        path: '*',
-        redirect: '/404'
+        path:'/notfound',
+        name:'notfound',
+        component:()=> import('../views/NotFound.vue')
     },
+    {
+        path:'/lessons',
+        name:'lessons',
+        component:()=> import('../views/Lessons.vue')
+    }
 ]
 
 
